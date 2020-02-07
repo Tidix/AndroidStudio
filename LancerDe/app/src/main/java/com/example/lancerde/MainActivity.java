c10c8f3345e8a8fdf38a903e445bfcfd26b8ad36
Damien <damien.lvzl@gmail.com>
2020-02-07 11:38:29 +0100

    1

---
+++ LancerDe/app/src/main/java/com/example/lancerde/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.lancerde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView score;
+    ImageView d1;
+    ImageView d2;
+    ImageView d3;
+    int vd1;
+    int vd2;
+    int vd3;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initiate();
+    }
+
+    public void bLancer(View v){
+        lancer(1);
+    }
+
+    public void lancer(int nde){
+        vd1 = (int)Math.random()%6 +1;
+        vd2 = (int)Math.random()%6 +1;
+        vd3 = (int)Math.random()%6 +1;
+
+
+    }
+
+
+    public void initiate(){
+        button = findViewById(R.id.bouton);
+        score = findViewById(R.id.score);
+        d1 = findViewById(R.id.d1);
+        d2 = findViewById(R.id.d2);
+        d3 = findViewById(R.id.d3);
+
+        d1.setImageResource(R.drawable.dice_1);
+        d2.setImageResource(R.drawable.dice_1);
+        d3.setImageResource(R.drawable.dice_1);
+    }
+}
